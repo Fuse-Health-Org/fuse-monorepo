d188e2e0f4ab57a7c192afbe0d6344b1d85c276b
Eduardo Hidalgo <rami.edu.hi@outlook.com>
2026-01-20 18:12:21 -0500

    added dark theme support and theme toggle functionality in the sidebar

--- fuse-affiliate-portal-frontend/pages/_app.tsx
+++ fuse-affiliate-portal-frontend/pages/_app.tsx
@@ -2,15 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { HeroUIProvider, ToastProvider } from '@heroui/react'
 import { AuthProvider } from '../contexts/AuthContext'
+import { ThemeProvider } from '../contexts/ThemeContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <HeroUIProvider>
-            <ToastProvider />
-            <AuthProvider>
-                <Component {...pageProps} />
-            </AuthProvider>
-        </HeroUIProvider>
+        <ThemeProvider>
+            <HeroUIProvider>
+                <ToastProvider />
+                <AuthProvider>
+                    <Component {...pageProps} />
+                </AuthProvider>
+            </HeroUIProvider>
+        </ThemeProvider>
     )
 }
 
